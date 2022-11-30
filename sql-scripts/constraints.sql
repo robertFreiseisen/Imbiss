c5971dd13699be2d09be04a8cfa14c28caa72b7a
Beko San <51796228+wolf1612@users.noreply.github.com>
2022-11-30 14:40:07 +0100

    Update constraints.sql

--- sql-scripts/constraints.sql
+++ sql-scripts/constraints.sql
@@ -1,1 +1,13 @@
 
+CREATE TABLE supplier
+( supplier_id numeric(10) NOT NULL,
+  supplier_name varchar2(50) NOT NULL,
+  contact_name varchar2(50),
+  CONSTRAINT supplier_unique UNIQUE (supplier_id, supplier_name)
+);
+
+ALTER TABLE Inventur 
+ADD CONSTRAINT constraint_name UNIQUE (column1, column2, ... column_n);
+
+ALTER TABLE Haus ADD CONSTRAINT haus_id_PM PRIMARY KEY
+ (haus_id);
